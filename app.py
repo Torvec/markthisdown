eb60f2301836f2f6362178e71d72f4720edd81b9
vonedward <me@edward-vonschondorf.dev>
2025-05-16 21:22:57 -0700

    Add Toast UI editor via cdn, tailwindcss via cdn, create dashboard page and route, create editor page and route

--- app.py
+++ app.py
@@ -3,32 +3,32 @@ from flask import Flask, render_template, request, redirect, url_for, session
 app = Flask(__name__)
 app.secret_key = "this-is-super-secret-no-sharing"
 
-@app.route("/", methods=["POST", "GET"])
-def home():
+@app.route("/")
+def dashboard():
+    return render_template("dashboard.html")
+
+@app.route("/editor", methods = ["POST", "GET"])
+def editor():
     err = None
     if request.method == "POST":
-        if valid_markdown(request.form["post"]):
-            session["post"] = request.form["post"]
-            session["post2"] = request.form["post2"]
+        if valid_markdown(request.form["markdownContent"]):
+            session["markdownContent"] = request.form["markdownContent"]
             return redirect(url_for('saved'))
         else:
             err = "Invalid Markdown"
             session["error"] = err
             return redirect(url_for('error'))
-    return render_template("index.html", error=err)
+    return render_template("editor.html", err=err)
 
 def valid_markdown(post):
     return len(post) > 0
 
 @app.route("/saved")
 def saved():
-    # post = session.get("post")
-    post = session.pop("post", None)
-    post2 = session.pop("post2", None)
-    return render_template("saved.html", post=post, post2=post2)
+    markdownContent = session.pop("markdownContent", None)
+    return render_template("saved.html", markdownContent=markdownContent)
 
 @app.route("/error")
 def error():
-    # error = session.get("error")
     error = session.pop("error", None)
     return render_template("error.html", error=error)
