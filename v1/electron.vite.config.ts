4f5db50434be09d10e683d709cf6bfe5c042cb53
Edward Vonschondorf <edvonschondorf@gmail.com>
2025-07-05 08:52:10 -0700

    Add electron forge to build the app

--- v1/electron.vite.config.ts
+++ v1/electron.vite.config.ts
@@ -1,14 +1,21 @@
 import { resolve } from "path";
 import { defineConfig, externalizeDepsPlugin } from "electron-vite";
 import react from "@vitejs/plugin-react";
+// @ts-expect-error: Ignore missing type declarations for @tailwindcss/vite
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   main: {
     plugins: [externalizeDepsPlugin()],
+    build: {
+      outDir: "dist/main",
+    },
   },
   preload: {
     plugins: [externalizeDepsPlugin()],
+    build: {
+      outDir: "dist/preload",
+    },
   },
   renderer: {
     resolve: {
@@ -17,5 +24,8 @@ export default defineConfig({
       },
     },
     plugins: [react(), tailwindcss()],
+    build: {
+      outDir: "dist/renderer",
+    },
   },
 });
